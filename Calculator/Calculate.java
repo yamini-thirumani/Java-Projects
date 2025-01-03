f019dc92a1f8f4d057b8259027f5ec9d814a5abd
yamini-thirumani <thirumaniyaminivk@gmail.com>
2025-01-03 19:20:37 +0530

    Updated Calculate.java with improved functionality

--- Calculator/Calculate.java
+++ Calculator/Calculate.java
@@ -1,40 +1,44 @@
 import java.util.*;
 
-class Calculate {
+class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Basic Calculator");
-        System.out.print("Enter the value a : ");
-        int a = sc.nextInt();
-        System.out.print("Enter the value b : ");
-        int b = sc.nextInt();
-        System.out.print("Enter the operation (character- '+','-','*','%','/') : ");
-        String op = sc.next();
-        char ch = op.charAt(0);
         
-        switch (ch) {
-            case '+': System.out.println("Addition of " + a + " and " + b + " = " + (a + b));
-                      break;
-            case '-': System.out.println("Subtraction of " + a + " and " + b + " = " + (a - b));
-                      break;
-            case '*': System.out.println("Product of " + a + " and " + b + " = " + (a * b));
-                      break;
+        while (true) {
+            System.out.print("Enter the value a: ");
+            int a = sc.nextInt();
+            System.out.print("Enter the operation (character- '+','-','*','%','/'): ");
+            char op = sc.next().charAt(0);
+            System.out.print("Enter the value b: ");
+            int b = sc.nextInt();
+            
+            try {
+                int result = performOperation(a, b, op);
+                System.out.println("Result: " + result);
+            } catch (ArithmeticException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+            
+            System.out.print("Do you want to continue? (yes/no): ");
+            String choice = sc.next();
+            if (!choice.equalsIgnoreCase("yes")) break;
+        }
+        
+        sc.close();
+    }
+
+    static int performOperation(int a, int b, char op) {
+        switch (op) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
             case '/': 
-                      if (b != 0) {
-                          System.out.println("Division of " + a + " and " + b + " = " + (a / b));
-                      } else {
-                          System.out.println("Error: Division by zero is not allowed.");
-                      }
-                      break;
+                if (b == 0) throw new ArithmeticException("Division by zero");
+                return a / b;
             case '%': 
-                      if (b != 0) {
-                          System.out.println("Modulo of " + a + " and " + b + " = " + (a % b));
-                      } else {
-                          System.out.println("Error: Modulo by zero is not allowed.");
-                      }
-                      break;
-            default: 
-                System.out.println("Enter a Valid Character/Operator.");
+                if (b == 0) throw new ArithmeticException("Modulo by zero");
+                return a % b;
+            default: throw new IllegalArgumentException("Invalid operator");
         }
     }
 }
